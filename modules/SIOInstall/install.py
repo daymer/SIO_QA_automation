e2cb4e63d774231f6225764acbfcb2e07db2909f
avramk <kirill.avramenko@emc.com>
2019-03-29 22:52:04 +0300

    Added auto install

--- modules/SIOInstall/install.py
+++ modules/SIOInstall/install.py
@@ -90,15 +90,17 @@ def create_cluster(nodes: list):
         elif each_node.role == 'tb':
             tbs.append(each_node)
             mode += 1
-    if 3 == mode < 5:
+    if 3 <= mode < 5:
         slave = managers[0]
         add_slave_mdm(master, slave)
         tb = tbs[0]
         add_tb(master, tb)
+        # TODO: change adding to use ID instead of names
         cmd_to_execute = 'scli --switch_cluster_mode --cluster_mode 3_node --add_slave_mdm_name MDM_{slave_name} ' \
-                         '--add_tb_ip TB_{tb_name}'.format(slave_name=','.join(slave.mgmt_ip),
+                         '--add_tb_name TB_{tb_name}'.format(slave_name=str(slave.mgmt_ip),
                                                            tb_name=str(tb.mgmt_ip))
         master.ssh_execute(cmd_to_execute=cmd_to_execute)
+    # TODO: add check if there is enough managers
     elif mode >= 5:
         slaves = []
         tiebrakers = []
@@ -107,6 +109,7 @@ def create_cluster(nodes: list):
             add_slave_mdm(master, slaves[i])
             tiebrakers.append(tbs[i])
             add_tb(master, tbs[i])
+        # TODO: change adding to use ID instead of names
         cmd_to_execute = 'scli --switch_cluster_mode --cluster_mode 5_node --add_slave_mdm_name MDM_{slave1},' \
                          'MDM_{slave2} --add_tb_name TB_{tb1},TB_{tb2}'.format(slave1=str(slaves[0].mgmt_ip),
                                                                                slave2=str(slaves[1].mgmt_ip),
@@ -121,6 +124,7 @@ def preinstall_cleanup(node: NodeInInstall):
 
 
 def install(nodes: list, build: str, debug: bool = True, signed: bool = False):
+    # TODO: Change up to cluster creation to use gevents
     cluster_mode = 0
     build_list = list(build)
     build_list = [char.replace('-', '.') for char in build_list]
@@ -161,14 +165,42 @@ def install(nodes: list, build: str, debug: bool = True, signed: bool = False):
     create_cluster(nodes)
 
 
+def auto_install(ips: list, build: str, mode: int = 1):
+    nodes = []
+    for each_ip in ips:
+        nodes.append(NodeInInstall(each_ip, sds=True, sdc=True))
+    if 3 <= mode < 5:
+        num_of_managers = 2
+        num_of_mdms = 3
+    elif mode >= 5:
+        num_of_managers = 3
+        num_of_mdms = 5
+    else:
+        num_of_managers = 1
+        num_of_mdms = 1
+    for i in range(num_of_managers):
+        nodes[i].is_manager = True
+    for i in range(num_of_mdms):
+        nodes[i].is_mdm = True
+    install(nodes, build)
+
+
+logger = logging.getLogger()
 nodelist = [
          NodeInInstall('10.234.210.125', manager=True, mdm=True, sds=True, sdc=True),
          NodeInInstall('10.234.210.126', manager=True, mdm=True, sds=True, sdc=True),
-         NodeInInstall('10.234.210.127', mdm=True, sds=True, sdc=True),
-         NodeInInstall('10.234.210.128'),
-         NodeInInstall('10.234.210.103')]
+         NodeInInstall('10.234.210.127', manager=True, mdm=True, sds=True, sdc=True),
+         NodeInInstall('10.234.210.128', mdm=True, sds=True, sdc=True),
+         NodeInInstall('10.234.210.103', mdm=True, sds=True, sdc=True)]
+
+ip_list = [
+    '10.234.210.125',
+    '10.234.210.126',
+    '10.234.210.127',
+    '10.234.210.128',
+    '10.234.210.103']
+
 
 siobuild = "3.0-0.769"
-logger = logging.getLogger()
-
-install(nodelist, siobuild)
+auto_install(ip_list, siobuild, 5)
+# install(list_of_nodes, siobuild)
